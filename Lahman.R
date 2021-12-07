462f586d75559c8708454c5a7101c8116617298a
Benjamin <spotswoodb@gmail.com>
2021-12-06 21:04:32 -0600

    Lahman dataset tutorial

--- Lahman.R
+++ Lahman.R
@@ -8,4 +8,15 @@ head(Teams)
 
 mydata <- Teams %>% select(yearID,lgID,teamID,W,L,R,RA) %>%
   filter(yearID==2014,lgID=="AL") %>%
-  mutate(wpct=R/(R+RA))
+  mutate(wpct=R^2/(R^2+RA^2),expwin=round(wpct*(W+L)),diff=W-expwin)
+
+head(mydata)
+
+mydata
+
+ggplot(mydata,aes(expwin,W)) + geom_point() + stat_smooth(method="lm")
+
+mod <- lm(W~expwin,data=mydata)
+mod
+
+summary(mod)
